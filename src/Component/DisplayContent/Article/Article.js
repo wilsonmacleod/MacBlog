b7a60f98d6db5dc54b3177f0f5137e9da10bd453
Wilson MacLeod <wilsonmacleod@gmail.com>
2020-02-10 12:01:21 -0800

    article text newlines, need to add indentation, bolding etc

--- src/Component/DisplayContent/Article/Article.js
+++ src/Component/DisplayContent/Article/Article.js
@@ -3,10 +3,15 @@ import React from 'react';
 import classes from './Article.module.css';
 
 const article = (props) => {
+    let content = props.text.split('\n');
+    content = 
+        content.map(c  =>
+            <p>{c}</p>
+        )
     return ( 
             <div className={classes.Body}>
                 <h1>{props.title}</h1>
-                <p>{props.text}</p>
+                <p>{content}</p>
             </div>
      );
 }
